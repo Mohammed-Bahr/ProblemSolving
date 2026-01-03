90826f239609acacd9e283b6663edd3baec66f79
Mohammed_Bahr <mohammedbahr686@gmail.com>
2026-01-03 21:11:32 +0200

    Q1. Concatenation of Array

--- Solutions/Q1. Concatenation of Array.py
+++ Solutions/Q1. Concatenation of Array.py
@@ -2,3 +2,12 @@ class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         res = nums + nums
         return res
+
+
+def main():
+    s = Solution()
+    print(s.getConcatenation([1, 2, 3]))
+
+
+if __name__ == "__main__":
+    main()
