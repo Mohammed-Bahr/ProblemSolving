e1bcc50572972544a26e36b097cfaee0e1b53ce8
Mohammed_Bahr <mohammedbahr686@gmail.com>
2026-01-17 20:36:07 +0200

    dummy

---
+++ Solutions/49. Group Anagrams.js
@@ -0,0 +1,38 @@
+
+function areMapsEqual(map1, map2) {
+    if (map1.size !== map2.size) return false;
+  
+    for (const [key, val] of map1) {
+      if (map2.get(key) !== val) return false;
+    }
+    return true;
+  }
+  
+  var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+  
+    const map = new Map();
+    const map1 = new Map();
+  
+    for (let c of s) {
+      map.set(c, (map.get(c) || 0) + 1);
+    }
+  
+    for (let c of t) {
+      map1.set(c, (map1.get(c) || 0) + 1);
+    }
+  
+    return areMapsEqual(map, map1);
+  };
+  
+var groupAnagrams = function(strs) {
+    for(let i = 2 ;i < strs.length() ; i+=2){
+        if(isAnagram(strs[i-1] , strs[i-2])){
+            
+        }
+    }
+};
+  
+
+console.log(isAnagram("anagram", "nagaram")); // true
+  
