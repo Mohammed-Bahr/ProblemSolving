c04c56f0967e164af29df3fdee2d23b361238ccb
Mohammed_Bahr <mohammedbahr686@gmail.com>
2026-01-19 21:25:56 +0200

    49. Group Anagrams

--- Solutions/49. Group Anagrams.js
+++ Solutions/49. Group Anagrams.js
@@ -1,38 +1,23 @@
-
-function areMapsEqual(map1, map2) {
-    if (map1.size !== map2.size) return false;
-  
-    for (const [key, val] of map1) {
-      if (map2.get(key) !== val) return false;
-    }
-    return true;
-  }
-  
-  var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-  
-    const map = new Map();
-    const map1 = new Map();
-  
-    for (let c of s) {
-      map.set(c, (map.get(c) || 0) + 1);
-    }
-  
-    for (let c of t) {
-      map1.set(c, (map1.get(c) || 0) + 1);
-    }
-  
-    return areMapsEqual(map, map1);
-  };
-  
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    for(let i = 2 ;i < strs.length() ; i+=2){
-        if(isAnagram(strs[i-1] , strs[i-2])){
-            
-        }
-    }
-};
-  
+    let ans = {};
 
-console.log(isAnagram("anagram", "nagaram")); // true
-  
+    for (let s of strs) {
+        let count = new Array(26).fill(0);
+
+        for (let c of s) {
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+
+        let key = count.join('#');
+        if (!ans[key]) {
+            ans[key] = [];
+        }
+        ans[key].push(s);
+    }
+
+    return Object.values(ans);    
+};
