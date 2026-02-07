459eaeace711b267771ba80bc6fd6e9b435b0584
Mohammed-Bahr <mohammedbahr686@gmail.com>
2026-02-07 11:40:46 +0200

    100. Same Tree

--- Solutions/125. Valid Palindrome.cpp
+++ Solutions/125. Valid Palindrome.cpp
@@ -1,22 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
+    bool isPalindrome(string s)
+    {
         queue<char> q;
         stack<char> st;
 
-        for (char c : s) {
-            if (isalpha(c) || isdigit(c)) {
+        for (char c : s)
+        {
+            if (isalpha(c) || isdigit(c))
+            {
                 char x = tolower(c);
                 q.push(x);
                 st.push(x);
             }
         }
 
-        while (!q.empty()) {
-            if (q.front() != st.top()) {
+        while (!q.empty())
+        {
+            if (q.front() != st.top())
+            {
                 return false;
             }
             q.pop();
@@ -27,8 +33,9 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution Sol;
     string s = "A man, a plan, a canal: Panama";
-    cout << Sol.isPalindrome(s); 
+    cout << Sol.isPalindrome(s);
 }
