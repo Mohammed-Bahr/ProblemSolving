2e474a9aca24c2c283143957b77eb6606c71e2bd
Mohammed-Bahr <mohammedbahr686@gmail.com>
2026-02-09 11:51:43 +0200

    102. Binary Tree Level Order Traversal && 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Solutions/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        output = []
+        queue = [root]
+        while queue:
+            size = len(queue)
+            level = []
+            for i in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            output.append(level)
+        return output
+    
+    
+    
+sol = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(sol.levelOrder(root)) 
+        
