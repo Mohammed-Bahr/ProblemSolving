e4c177e1bdd3b3b909ce56186715288c4ec562eb
Mohammed-Bahr <mohammedbahr686@gmail.com>
2026-08-23 04:29:48 +0300

    Valid Palindrome II.cpp

---
+++ Solutions/CPP/Valid Palindrome II.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Try removing either the left or right character
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    string s = "abca";
+
+    bool result = solution.validPalindrome(s);
+
+    cout << boolalpha << result << endl;
+
+    return 0;
+}
